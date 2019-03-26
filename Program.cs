9420123f9204cec7cd4ac2a12e06b26ebc773861
akhmad0710 <you@example.com>
2019-03-26 14:54:57 +0700

    latihanmemberclass

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanmemberclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+
+            taxi taxi = new taxi();
+
+            //pengesetan nilai properties
+
+            taxi.drivername = "jono";
+            taxi.onduty = true;
+            taxi.numpassenger = 10;
+
+            //pemanggil method
+            taxi.TaxiInfo();
+            taxi.PickupPassenger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanmemberclass
+{
+    class taxi
+    {
+        //properties
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public float numpassenger { get; set; }
+
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("drivername = {0}", drivername );
+            Console.WriteLine("onduty = {0}", onduty);
+            Console.WriteLine("numpassenger = {0}", numpassenger);
+        }
+
+        public void PickupPassenger()
+        {
+            Console.WriteLine("jono sedang menjemput penumpang");
+        }
+
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("jonoselesai mengantar penumpang");
+        }
+
+ 
+
+    }
+}
